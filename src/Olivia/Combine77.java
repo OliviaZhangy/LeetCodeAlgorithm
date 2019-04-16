afd1ddf5947827d783fdf7d9c1a6c4bef8d41f9f
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-04-16 12:40:52 -0700

    N0 17 77 78
    BFS complete

--- src/Olivia/Combine77.java
+++ src/Olivia/Combine77.java
@@ -1,24 +1,55 @@
 package Olivia;
 
+//NO.77
+//Try using BFS
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Combine77 {
+//    public List<List<Integer>> combine(int n, int k) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        combineHelper(n, k, result, new ArrayList<>(), 1);
+//        return result;
+//    }
+//
+//    private void combineHelper(int n, int k, List<List<Integer>> result, List<Integer> tmp, int startLoc) {
+//        if (k == 0) {
+//            result.add(new ArrayList<>(tmp));
+//        }
+//        for (int i = startLoc; i <= n; i++) {
+//            tmp.add(i);
+//            /* Be careful about how to recursive */
+//            combineHelper(n, k - 1, result, tmp, i + 1);//startLoc should be i+1 rather than startLoc+1
+//            tmp.remove(tmp.size() - 1);
+//        }
+//    }
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        combineHelper(n, k, result, new ArrayList<>(), 1);
+        //corner case
+        if(n==0||k==0){
+            return result;
+        }
+        List<Integer> cur = new ArrayList<>();
+        combineHelper(n,k,result,cur,1);
         return result;
+
+}
+//a helper function to help find the combination
+    private void combineHelper(int n, int k, List<List<Integer>> result, List<Integer> cur,int startLoc){
+        if (k==0){
+            result.add(new ArrayList<Integer>(cur));
+            return;
+        }
+        for(int i = startLoc;i<=n;i++){
+            cur.add(i);
+            combineHelper(n,k-1,result,cur,i+1);
+            cur.remove(cur.size()-1);
+
+        }
+
+
     }
 
-    private void combineHelper(int n, int k, List<List<Integer>> result, List<Integer> tmp, int startLoc) {
-        if (k == 0) {
-            result.add(new ArrayList<>(tmp));
-        }
-        for (int i = startLoc; i <= n; i++) {
-            tmp.add(i);
-            /* Be careful about how to recursive */
-            combineHelper(n, k - 1, result, tmp, i + 1);//startLoc should be i+1 rather than startLoc+1
-            tmp.remove(tmp.size() - 1);
-        }
-    }
 }
+
--- src/Olivia/Subsets78.java
+++ src/Olivia/Subsets78.java
@@ -4,22 +4,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+//NO78 USING BACKTRACKING
+
 public class Subsets78 {
+//    public List<List<Integer>> subsets(int[] nums) {
+//        List<List<Integer>> result = new ArrayList<>();
+//        if (nums.length == 0)
+//            return result;
+//        Arrays.sort(nums);
+//        subsetHelper(nums, result, new ArrayList<>(), 0);
+//        return result;
+//    }
+//
+//    private void subsetHelper(int[] nums, List<List<Integer>> result, List<Integer> tmp, int startLoc) {
+//        result.add(new ArrayList<>(tmp));
+//        for (int i = startLoc; i < nums.length; i++) {
+//            tmp.add(nums[i]);
+//            subsetHelper(nums, result, tmp, i + 1);
+//            tmp.remove(tmp.size() - 1);
+//        }
+//    }
+
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        if (nums.length == 0)
-            return result;
+        List<Integer> cur = new ArrayList<>();
+
+        //corner case
+        if(nums.length==0) return result;
+        //need to sort nums first
         Arrays.sort(nums);
-        subsetHelper(nums, result, new ArrayList<>(), 0);
+        subsetHelper(nums,result,cur,0);
         return result;
     }
-
-    private void subsetHelper(int[] nums, List<List<Integer>> result, List<Integer> tmp, int startLoc) {
-        result.add(new ArrayList<>(tmp));
-        for (int i = startLoc; i < nums.length; i++) {
-            tmp.add(nums[i]);
-            subsetHelper(nums, result, tmp, i + 1);
-            tmp.remove(tmp.size() - 1);
+    // a helper function to help find the subsets
+    private void subsetHelper(int[] nums,List<List<Integer>> result, List<Integer> cur, int startLoc){
+        result.add(new ArrayList<Integer>(cur));
+        for(int i=startLoc;i<nums.length;i++){
+            cur.add(nums[i]);
+            subsetHelper(nums,result,cur,i+1);
+            cur.remove(cur.size()-1);
         }
     }
 }
