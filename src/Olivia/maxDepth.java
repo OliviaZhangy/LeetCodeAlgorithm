449f35e42a33c7c856295019233515d9ca08fd37
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-04-29 21:42:00 -0700

    NO 104
    tree problem

---
+++ src/Olivia/maxDepth.java
@@ -0,0 +1,20 @@
+package src.Olivia;
+
+//NO 104
+//tree
+
+public class maxDepth {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+
+}
