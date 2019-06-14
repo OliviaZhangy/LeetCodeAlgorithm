266f97a87bd44854638be6d1ab4f98e446a2c41d
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-06-14 11:05:18 -0700

    NO 300
    search:LIS

---
+++ src/Olivia/increasingTriplet.java
@@ -0,0 +1,6 @@
+package src.Olivia;
+
+//NO 
+
+public class increasingTriplet {
+}
--- src/Olivia/lengthOfLIS.java
+++ src/Olivia/lengthOfLIS.java
@@ -6,6 +6,25 @@ package src.Olivia;
 
 public class lengthOfLIS {
     public int lengthOfLIS(int[] nums) {
+        int[] tails = new  int[nums.length];
+        int size = 0;
+        for(int x:nums){
+          int start = 0;
+          int end = size;
+          while(start!=end){
+              int mid = start +(end-start)/2;
+              if(tails[mid]<x){
+                  start = mid + 1;
+              }
+              else{
+                  end = mid;
+              }
+          }
+              tails[start] = x;
+              if(start==size) size++;
+
+        }
+        return size;
 
     }
 
