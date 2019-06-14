6eb4279567fa64927e622304c872c335dadfe17f
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-06-13 17:38:54 -0700

    NO 278
    search

---
+++ src/Olivia/lengthOfLIS.java
@@ -0,0 +1,12 @@
+package src.Olivia;
+
+//NO 300
+//search
+//https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/JavaPython-Binary-search-O(nlogn)-time-with-explanation
+
+public class lengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+
+    }
+
+}
