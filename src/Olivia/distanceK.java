6dc8bdc9e1544ab31f4e52b466d0a97f181539fa
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-08-13 10:59:10 -0700

    NO 863
    BFS AND GRAPH

---
+++ src/Olivia/distanceK.java
@@ -0,0 +1,62 @@
+package src.Olivia;
+
+import java.util.*;
+
+public class distanceK {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        List<Integer> res = new ArrayList<>();
+        if(root==null||K<0) return res;
+        //construct a map to store the node as the key and parent and children as the value
+        Map<TreeNode,List<TreeNode>> map = new HashMap<>();
+        buildHelper(map,root,null);
+        if(!map.containsKey(target)) return res;
+        // a set that contains the tree node visited !!
+        Set<TreeNode> visited = new HashSet<>();
+        Queue<TreeNode> que = new LinkedList<>();
+        que.add(target);
+        visited.add(target);
+        //do BFS
+        while (!que.isEmpty()){
+            int size = que.size();
+            if(K==0){
+                for(int i=0;i<size;i++){
+                    res.add(que.poll().val);
+                }
+                return res;
+            }
+            for(int i=0;i<size;i++){
+                TreeNode cur = que.poll();
+                for(TreeNode tmp:map.get(cur)){
+                    if(visited.contains(tmp)) continue;
+                    visited.add(tmp);
+                    que.add(tmp);
+                }
+            }
+            K--;
+        }
+        return res;
+
+    }
+    private void buildHelper(Map<TreeNode,List<TreeNode>> map, TreeNode root, TreeNode parent){
+        //base case
+        if(root==null) return;
+        if(!map.containsKey(root)){
+            map.put(root,new ArrayList<>());
+
+            if(parent!=null){
+                //add the parent to the root
+                map.get(root).add(parent);
+                //add the root to the parent
+                map.get(parent).add(root);
+            }
+            buildHelper(map,root.left,root);
+            buildHelper(map,root.right,root);
+        }
+    }
+}
--- src/Olivia/removeStones.java
+++ src/Olivia/removeStones.java
@@ -1,9 +1,38 @@
 package src.Olivia;
 
 //NO 947
+//union find
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class removeStones {
+    Map<Integer,Integer> map  = new HashMap<>();
+    int island = 0;
     public int removeStones(int[][] stones) {
-
+        for(int i=0;i<stones.length;i++){
+            unionfind(stones[i][0],10000+stones[i][1]);
+        }
+        return stones.length-island;
+    }
+    private void unionfind(int x,int y){
+       x = find(x);
+       y = find(y);
+       //union
+       if(map.get(x)!=y){
+           map.put(x,y);
+           island--;
+       }
+    }
+    private int find(int x){
+        if(!map.containsKey(x)) {
+            map.put(x,x);
+            island++;
+        }
+       if(x!=map.get(x)){
+           //recursive to find father
+           map.put(x,find(map.get(x)));
+       }
+       return map.get(x);
     }
 }
