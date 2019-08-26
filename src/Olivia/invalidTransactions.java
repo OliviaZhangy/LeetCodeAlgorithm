c8a135ed35176b15d13f289bcd42d9279d9ce2e3
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-08-26 16:49:23 -0700

    No 1171
    understand why start from dummy

--- src/Olivia/invalidTransactions.java
+++ src/Olivia/invalidTransactions.java
@@ -3,10 +3,13 @@ package src.Olivia;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class invalidTransactions {
+
     public List<String> invalidTransactions(String[] transactions) {
         List<String> res = new ArrayList<>();
-        return null;
+
 
     }
 }
--- src/Olivia/removeZeroSumSublists.java
+++ src/Olivia/removeZeroSumSublists.java
@@ -1,5 +1,12 @@
 package src.Olivia;
 
+
+import java.util.HashMap;
+import java.util.List;
+
+//NO 1171
+//calculate the sum of the previous one
+//whenever meet the same sum, remove all the elements between them
 public class removeZeroSumSublists {
     public class ListNode {
      int val;
@@ -11,7 +18,25 @@ public class removeZeroSumSublists {
         if(head==null) return head;
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-return null;
+        HashMap<Integer,ListNode> map = new HashMap<>();
+        int preSum = 0;
+        //it should be start from dummy ([1,-1]--> 0,1,0 so return [])
+        ListNode cur = dummy;
+
+        while (cur!=null){
+            preSum += cur.val;
+            if(!map.containsKey(preSum)){
+                map.put(preSum,cur);
+            }
+            else{
+                //connect together
+                ListNode pre = map.get(preSum);
+                pre.next = cur.next;
+
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
     }
 
 }
