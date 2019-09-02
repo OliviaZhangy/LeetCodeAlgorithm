5a2d32cc7f45cf4f4770186a9a5d99e451c534e8
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-09-02 10:01:40 -0700

    No 238 239

---
+++ src/Olivia/maxSlidingWindow.java
@@ -0,0 +1,14 @@
+package src.Olivia;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.TreeSet;
+
+//No 239
+public class maxSlidingWindow {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        TreeSet<Integer> set = new TreeSet<>();
+        
+
+    }
+}
--- src/Olivia/productExceptSelf.java
+++ src/Olivia/productExceptSelf.java
@@ -1,4 +1,23 @@
 package src.Olivia;
 
+//NO 238
+//no division
+
 public class productExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        //multiply from left to right
+        for(int i=1;i<nums.length;i++){
+            res[i] = res[i-1]*nums[i-1];
+        }
+        //multiply from right to left
+        //use right to store the multiply value
+        int right = 1;
+        for(int i= nums.length-2;i>=0;i--){
+            right = right*nums[i+1];
+            res[i] = res[i]*right;
+        }
+        return res;
+    }
 }
