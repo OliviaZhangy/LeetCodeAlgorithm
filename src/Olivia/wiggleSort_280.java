26b5106b32cfd48ee9af65ce04b6c01e24f4e136
OliviaZhangy <Olivia_zhangy@outlook.com>
2019-09-10 14:05:42 -0700

    NO 280

--- src/Olivia/wiggleSort_280.java
+++ src/Olivia/wiggleSort_280.java
@@ -3,4 +3,22 @@ package src.Olivia;
 //NO 280
 
 public class wiggleSort_280 {
+    public void wiggleSort(int[] nums) {
+            for(int i=0;i<nums.length;i++){
+                if(i%2==1){
+                    if(nums[i]<nums[i-1])
+                        swap(nums,i);
+                }
+                else{
+                    if(i!=0&&nums[i]>nums[i-1])
+                        swap(nums,i);
+                }
+            }
+        }
+        private void swap(int[] nums, int i){
+            int tmp = nums[i];
+            nums[i] = nums[i-1];
+            nums[i-1] = tmp;
+        }
+    }
 }
